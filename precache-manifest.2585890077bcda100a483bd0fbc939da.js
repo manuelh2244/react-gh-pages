23794660bacf7d0caf364f58e1f0c0568b4fac13
Manuel <manuel.heyers@gmail.com>
2020-05-27 23:17:35 -0400

    Updates

---
+++ precache-manifest.2585890077bcda100a483bd0fbc939da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcdbc397b9a679b2edeef2b5ee2d041c",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "5d0699b69b8c79e784b0",
+    "url": "/react-gh-pages/static/css/main.0bea54da.chunk.css"
+  },
+  {
+    "revision": "61863e98a6e6b2bdfd2d",
+    "url": "/react-gh-pages/static/js/2.b38e5db1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.b38e5db1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d0699b69b8c79e784b0",
+    "url": "/react-gh-pages/static/js/main.73b1ae50.chunk.js"
+  },
+  {
+    "revision": "50a8be1e1ef6eaa4cfb7",
+    "url": "/react-gh-pages/static/js/runtime-main.79c6e576.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
